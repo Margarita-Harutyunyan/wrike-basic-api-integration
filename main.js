5dec4b447c7ca45e8d4eb856fd9d9dc5f1fd30c1
Maga <magaharutyunyan969@gmail.com>
2024-08-19 19:12:57 +0400

    Inital get request and saving to a file

---
+++ main.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const axios = require('axios');
+const fs = require('fs');
+
+const token = process.env.WRIKE_API_TOKEN;
+const url = 'https://www.wrike.com/api/v4/tasks';
+
+
+axios.get(url, {
+  headers: {
+    'Authorization': `Bearer ${token}`,
+    'Content-Type': 'application/json'
+  }
+})
+  .then(response => {
+    const data = JSON.stringify(response.data, null, 2);
+
+    fs.writeFile('tasks.json', data, (err) => {
+      if (err) {
+        console.error('Error writing to file', err);
+      } else {
+        console.log('Successfully wrote to file');
+      }
+    });
+  })
+  .catch(error => {
+    console.error('There was an error with the axios request:', error);
+  });
+
+
